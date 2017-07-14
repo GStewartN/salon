d5e0d66b76396854a5099b754d40c832f809feff
Nathan Stewart <stewart.nathang@gmail.com>
2017-07-14 14:21:08 -0700

    fix app breaking typo

--- app/app.php
+++ app/app.php
@@ -25,15 +25,15 @@
         return $app['twig']->render('stylists.html.twig', array('stylists' => Stylist::getAll()));
     });
 
+    $app->get("/stylists/{id}", function($id) use ($app) {
+      $stylist = Stylist::find($id);
+      return $app['twig']->render('stylist.html.twig', array('stylist' => $stylist));
+    });
+
     $app->post("/stylists", function() use ($app) {
         $stylist = new Stylist($_POST['name']);
         $stylist->save();
         return $app['twig']->render('stylists.html.twig', array('stylists' => Stylist::getAll()));
-    });
-
-    $app->get("/stylists/{id}", function($id) use ($app) {
-        $stylist = Stylist::find($id);
-        return $app['twig']->render('stylist.html.twig', array('stylist' => $stylist));
     });
 
     $app->get("/stylists/{id}/edit", function($id) use ($app) {
@@ -41,7 +41,7 @@
         return $app['twig']->render('stylist_edit.html.twig', array('stylist' => $stylist));
     });
 
-    $app->patch("/categories/{id}", function($id) use ($app) {
+    $app->patch("/stylists/{id}", function($id) use ($app) {
         $name = $_POST['name'];
         $stylist = Stylist::find($id);
         $stylist->update($name);
@@ -52,6 +52,11 @@
         $stylist = Stylist::find($id);
         $stylist->delete();
         return $app['twig']->render('index.html.twig', array('stylists' => Stylist::getAll()));
+    });
+
+    $app->post("/delete_stylists", function() use ($app) {
+      Stylist::deleteAll();
+      return $app['twig']->render('index.html.twig');
     });
 
     return $app;
